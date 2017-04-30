69b3268b469da1d88396e6fc0483b03f415ec0d9
acer1\acer <grudacheva.t@gmail.com>
2017-04-30 20:54:58 +0300

    no message

--- js/script.js
+++ js/script.js
@@ -12,19 +12,3 @@ var MyLastName = 'Грудачева';
 var Space = ' ';
 
 console.log(MyName + Space + MyLastName);
-
-function click_b(){
-    alert("Привет, я - JS");
-    return false;
-}
-
-function animation(){
-    var img1 = document.getElementById('img1');
-    var img2 = document.getElementById('img2');
-    var rightBlock = document.getElementById('rightBlock');
-    
-    img1.className += "from-left-to-right-go";
-
-    setTimeout(function(){img2.className += ' from-left-to-right-go';}, 200);
-    rightBlock.className += " from-left-to-right-go";   
-}
